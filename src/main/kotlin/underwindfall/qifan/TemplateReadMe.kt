7a4a198d8e94676cb669afeaa1221264f5087bcd
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-11-27 00:46:52 +0100

    add github feeds sketlon

---
+++ src/main/kotlin/underwindfall/qifan/TemplateReadMe.kt
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2020 by Qifan YANG (@underwindfall)
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package underwindfall.qifan
+
+fun createReadMe(githubContent: List<FeedItem>, codeTimeContent: List<FeedItem>): String {
+  return """
+  <h2> Hi 👋, I'm Qifan ! </h2>
+  <p><em>Software Enginner @<a href="https://www.netatmo.com">Netatmo</a> 🔭
+  </em></p>
+  <table><tr><td valign="top" width="60%" rowspan="2">
+
+   ## Github Activity
+   <!-- githubActivity starts -->
+${githubContent.joinToString("\n\n") { "    $it" }}}
+   <!-- githubActivity ends -->
+   </td><td>
+   
+   ## Something about me
+   <!-- profile starts -->
+   ```kotlin
+   data class underwindfall(
+        val pronouns: String = "he/him"
+   )
+   ```
+   <!-- profile starts -->
+   </td></tr><tr><td>
+
+   ## 🏊‍♂️ <a href="https://gist.github.com/underwindfall/377ee88ba1fabd1e93516e48ca9c61eb" target="_blank">Weekly Development Breakdown</a>
+    <!-- codeTime starts -->
+    ```text
+${codeTimeContent.joinToString("\n\n") { "    $it" }}
+    ```
+    <!-- codeTime starts -->
+    </td></tr></table>
+  """.trimIndent()
+}
---
+++ src/main/kotlin/underwindfall/qifan/UpdateReadMe.kt
@@ -0,0 +1,102 @@
+/**
+ * Copyright (C) 2020 by Qifan YANG (@underwindfall)
+ *
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package underwindfall.qifan
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.types.file
+import com.squareup.moshi.Moshi
+import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
+import kotlin.system.exitProcess
+
+class UpdateReadmeCommand : CliktCommand() {
+    val outputFile by option("-o", help = "The README.md file to write")
+            .file()
+            .required()
+
+    override fun run() {
+        val okHttpClient = OkHttpClient.Builder()
+                .build()
+        val githubActivity = fetchGithubActivity(okHttpClient)
+        val list = listOf(
+                FeedItem("Markdown           31 mins  ████████████████████▒░░░  71.1%\n"),
+                FeedItem("Bash               10 mins  █████████▒░░░░░░░░░░░░░░  24.6%\n"),
+                FeedItem("JavaScript          0 secs  ███▓░░░░░░░░░░░░░░░░░░░░   1.4%\n"),
+                FeedItem("HTML                0 secs  ███▓░░░░░░░░░░░░░░░░░░░░   1.0%\n"),
+                FeedItem("Other               0 secs  ███▓░░░░░░░░░░░░░░░░░░░░   0.8%\n"),
+                FeedItem("Java                0 secs  ███▓░░░░░░░░░░░░░░░░░░░░   0.7%"))
+
+//        val codeTimeActivity = fetchCodeTimeActivity(okHttpClient)
+        val newReadMe = createReadMe(githubActivity, list)
+        outputFile.writeText(newReadMe)
+
+        exitProcess(0)
+    }
+}
+
+private fun fetchGithubActivity(client: OkHttpClient): List<FeedItem> {
+    val moshi = Moshi.Builder().build()
+    val githubApi = GitHubApi.create(client, moshi)
+    val activity = runBlocking { githubApi.getUserActivity("underwindfall") }
+    return activity
+            .filter { it.public }
+            .mapNotNull { event ->
+                when (val payload = event.payload) {
+                    UnknownPayload, null -> return@mapNotNull null
+                    is IssuesEventPayload -> {
+                        FeedItem(
+                                "${payload.action} issue [#${payload.issue.number}](${payload.issue.url}) on ${event.repo?.markdownUrl()}: \"${payload.issue.title}\"",
+                                event.createdAt
+                        )
+                    }
+                    is IssueCommentEventPayload -> {
+                        FeedItem(
+                                "commented on [#${payload.issue.number}](${payload.comment.htmlUrl}) in ${event.repo?.markdownUrl()}",
+                                event.createdAt
+                        )
+                    }
+                    is PushEventPayload -> {
+                        FeedItem(
+                                payload.commitMessage(event),
+                                event.createdAt
+                        )
+                    }
+                    is PullRequestPayload -> {
+                        FeedItem(
+                                "${payload.action} PR [#${payload.number}](${payload.pullRequest.url}) to ${event.repo?.markdownUrl()}: \"${payload.pullRequest.title}\"",
+                                event.createdAt
+                        )
+                    }
+                    is CreateEvent -> {
+                        FeedItem(
+                                "created ${payload.refType}${payload.ref?.let { " \"$it\"" } ?: ""} on ${event.repo?.markdownUrl()}",
+                                event.createdAt
+                        )
+                    }
+                    is DeleteEvent -> {
+                        FeedItem(
+                                "deleted ${payload.refType}${payload.ref?.let { " \"$it\"" } ?: ""} on ${event.repo?.markdownUrl()}",
+                                event.createdAt
+                        )
+                    }
+                }
+            }
+            .take(10)
+}
+
+fun main(argv: Array<String>) {
+    UpdateReadmeCommand().main(arrayOf("-o README.md"))
+}
